0c0733626d11f7d4e42882ebd412e39e92f45527
Juliana Pirolla <110180671+jpirolla@users.noreply.github.com>
2023-04-04 17:16:59 -0300

    Update delicious_knn.py
    commit atualização

--- delicious_knn.py
+++ delicious_knn.py
@@ -1,339 +1,288 @@
-# %% [markdown]
-# # Base Delicious - KNN
-
-# %% [markdown]
-# # 0. Estruturação do dataframe
-
-# %%
-import csv
-import numpy as np
 import pandas as pd
-import seaborn as sns 
-import sklearn as skl
-import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io import arff
-from sklearn.metrics import jaccard_score
+from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
-from sklearn.neighbors import kneighbors_graph, radius_neighbors_graph
+import sklearn as skl
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
 
 # %%
-# criando um dataframe do arquivo lido pelo pandas 
 df = pd.read_csv('cometa_csv/delicious.csv')
 
-# selecionando as tags
+# %%
 m_delicious = df.loc[:, "TAG_.imported":"TAG_youtube"]
 
-# uma vez que a distância entre as instâncias é calculada por comparação entre colunas, transponho a m_flags 
+# %%
 mt_delicious = np.array(m_delicious).T
-
-# configurando as casas decimais que irão ser impressas 
-np.set_printoptions(precision=3)
+display(mt_delicious)
 
 # %% [markdown]
-# # 1. Definindo funções que serão utilizadas 
+# ### Analisando dados (maior/menor)
 
 # %% [markdown]
-# - **draw_graph**: utilizada para gerar o grafo completo a partir da da matriz de distância usando como métrica a distância inserida
-#     - Cada elemento na matriz representa a distância (por exemplo Jaccard) entre duas amostras específicas.
+# #### Verificando quem é o menor valor diferente de zero
+# A máscara booleana é criada usando o operador != para comparar cada elemento do ndarray com zero. O resultado é um array booleano com valores True para os elementos diferentes de zero e False para os elementos iguais a zero.
+
+# %%
+mask = m_euclidiana!= 0
+min_value = np.min(m_euclidiana[mask])
+print(min_value)
+
+# %%
+mask = m_euclidiana!= 1
+max_value = np.max(m_euclidiana[mask])
+print(max_value)
+
+# %%
+mask = m_jaccard != 0
+min_value = np.min(m_jaccard[mask])
+print(min_value)
+
+# %%
+mask = m_jaccard != 1
+max_value = np.max(m_jaccard[mask])
+print(max_value)
+
+# %%
+median = (max_value-min_value)/2
+median
+
+# %% [markdown]
+# ## Kmeans usando distância Euclidiana
+
+# %%
+m_euclidiana = skl.metrics.pairwise_distances(mt_delicious, metric='euclidean')
+m_euclidiana
+
+# %% [markdown]
+# #### Recorrendo ao Kmeans ++ para otimizar a escolha do centroide
+
+# %%
+# metodo cotovelo 
+wcss = []
+for i in range(1,11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 50)
+    kmeans.fit(m_euclidiana)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11), wcss)
+plt.title('Método do cotovelo')
+plt.xlabel('Número de clusters')
+plt.ylabel('Within-Clister-Sum-of-Squares')
+plt.show()
+
+# %% [markdown]
+# #### Silhueta 
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html
+
+# %%
+# Silhueta
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+for i in range(2, 11):
+    clusterer = KMeans(n_clusters=i)
+    preds = clusterer.fit_predict(m_euclidiana)
+    score = silhouette_score(m_euclidiana, preds)
+    print('Silhueta para ' + str(i) + ' clusters : ' + str(score))
+
+# %% [markdown]
+# #### Supondo k=2
+
+# %%
+kmeans = KMeans(n_clusters = 2, init='k-means++', max_iter=300, n_init=30)
+clusters = kmeans.fit_predict(m_euclidiana)
+clusters  # consigo ver quem pertence a quais conjuntos
+
+# %%
+x = m_euclidiana
+
+plt.figure(figsize=(8, 8))
+plt.scatter(
+    x[clusters == 0,0], x[clusters == 0,1],
+    s=40,c='cyan',
+    #edgecolor='black',
+    label='Cluster A'
+)
+
+plt.scatter(
+    x[clusters == 1,0], x[clusters == 1,1],
+    s=40,c='blue',
+    #edgecolor='black',
+    label='Cluster B'
+)
+
+# centroide
+plt.scatter(
+    kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], 
+    s=90,c='red',
+    #edgecolor='black',
+    label='centroides'
+)
+
+plt.title('K-means utilizando distância euclidiana e k=3 na base Delicious.')
+plt.legend()
+plt.show()
+
+# %% [markdown]
+# #### Supondo k=3
+
+# %%
+kmeans = KMeans(n_clusters = 3, init='k-means++', max_iter=300, n_init=30)
+clusters = kmeans.fit_predict(m_euclidiana)
+clusters  # consigo ver quem pertence a quais conjuntos
+
+# %% [markdown]
+# #### Duvidas
+# - Devo normalizar os dados? A justificativo seria para que a variação em um atributo não ofusque as variações em outros atributos.
 # 
-# - **draw_knn_graph**: utilizada para gerar o grafo esparsificado por knn
-#     - o grafo é gerado tendo como base uma matriz esparsa 
-#     - O argumento k especifica o número de vizinhos mais próximos a serem considerados para cada amostra. 
-#     - O argumento mode especifica se a distância entre as amostras será armazenada como uma distância ou como uma similaridade. Se o mode for definido como 'distance', então as distâncias serão armazenadas na matriz. 
-#     - O argumento metric especifica a métrica de distância a ser usada para calcular a distância entre as amostras. 
-#     - O argumento include_self especifica se cada amostra será incluída como um dos seus próprios vizinhos mais próximos.
-#     
 
 # %%
-# criar o grafo completo
-def draw_graph(metric, color, matriz):
-    m = skl.metrics.pairwise_distances(matriz, metric=metric)
-    G = nx.from_numpy_array(m)
-    list(nx.selfloop_edges(G))
-    G.remove_edges_from(nx.selfloop_edges(G))
+x = m_euclidiana
 
-    plt.figure(num=None, figsize=(10, 10), dpi=80)
-    nx.draw(G, with_labels=False, node_color=color, node_size = 100) 
-    plt.title(f'Base "Delicious" - Grafo completo usando {metric}', fontsize=17)
-    plt.show()
+plt.figure(figsize=(8, 8))
+plt.scatter(
+    x[clusters == 0,0], x[clusters == 0,1],
+    s=40,c='cyan',
+    #edgecolor='black',
+    label='Cluster A'
+)
 
-# %%
-def draw_knn_graph(k, metric, cor, matriz):
-    sparse_matrix_knn = kneighbors_graph(matriz, k, mode='distance', metric=metric, include_self=False)
-    G_sparse_knn = nx.from_numpy_array(sparse_matrix_knn)
-    G_sparse_knn.remove_edges_from(nx.selfloop_edges(G_sparse_knn))
+plt.scatter(
+    x[clusters == 1,0], x[clusters == 1,1],
+    s=40,c='blue',
+    #edgecolor='black',
+    label='Cluster B'
+)
+plt.scatter(
+    x[clusters == 2,0], x[clusters == 2,1],
+    s=40,c='greenyellow',
+    #edgecolor='black',
+    label='CLuster C'
+)
 
-    plt.figure(num=None, figsize=(10, 10), dpi=80)
-    nx.draw(G_sparse_knn, with_labels=False, node_color=cor, node_size = 100, edgecolors='gray') 
-    plt.title(f'Base "Delicious" - Grafo esparsificado por KNN usando {metric} e k={k}', fontsize=17)
-    plt.show()
+# centroide
+plt.scatter(
+    kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], 
+    s=90,c='red',
+    #edgecolor='black',
+    label='centroides'
+)
+
+plt.title('K-means utilizando distância euclidiana e k=3 na base Delicious.')
+plt.legend()
+plt.show()
 
 # %% [markdown]
-# # 2. Aplicando Knn para as distâncias definidas 
+# ## Implementando para outras distâncias 
+# 
 
 # %% [markdown]
-# ## 2.1) Jaccard
+# ### Jaccard
+
+# %%
+m_jaccard = skl.metrics.pairwise_distances(mt_delicious, metric='jaccard')
+m_jaccard
+
+# %%
+# metodo cotovelo 
+wcss_jaccard = []
+for i in range(1,13):
+    kmeans_jaccard = KMeans(n_clusters = i, init = 'k-means++', max_iter = 400, n_init = 30)
+    kmeans_jaccard.fit(m_jaccard)
+    wcss_jaccard.append(kmeans_jaccard.inertia_)
+
+plt.plot(range(1,13), wcss_jaccard)
+plt.title('Método do cotovelo')
+plt.xlabel('Número de clusters')
+plt.ylabel('Within-Clister-Sum-of-Squares')
+plt.show()
+
+# %%
+for i in range(2, 11):
+    clusterer = KMeans(n_clusters=i)
+    preds = clusterer.fit_predict(m_jaccard)
+    score = silhouette_score(m_jaccard, preds)
+    print('Silhueta para ' + str(i) + ' clusters : ' + str(score))
 
 # %% [markdown]
-# #### Grafo completo
-
-# %%
-draw_graph('jaccard', 'Red', mt_delicious)
+# ### Dúvida:
+# - Não tende a zero tal como no caso da distância euclidiana -> jaccard é aplicável ainda sim?
 
 # %% [markdown]
-# #### Grafo esparsificado (knn)
+# ### Tanimoto
 
 # %% [markdown]
-# ##### 1-10
+# #### Cotovelo para achar k
 
 # %%
-draw_knn_graph(1,'jaccard','Red', mt_delicious)
-draw_knn_graph(2,'jaccard','Red', mt_delicious)
-draw_knn_graph(3,'jaccard','Red', mt_delicious)
-draw_knn_graph(4,'jaccard','Red', mt_delicious)
-draw_knn_graph(5,'jaccard','Red', mt_delicious)
+m_tanimoto = skl.metrics.pairwise_distances(mt_delicious, metric='rogerstanimoto')
+m_tanimoto
 
 # %%
-draw_knn_graph(6,'jaccard', 'Red',mt_delicious)
-draw_knn_graph(7,'jaccard','Red', mt_delicious)
-draw_knn_graph(8,'jaccard','Red', mt_delicious)
-draw_knn_graph(9,'jaccard','Red', mt_delicious)
-draw_knn_graph(10,'jaccard','Red', mt_delicious)
+# metodo cotovelo 
+wcss_tanimoto = []
+for i in range(1,13):
+    kmeans_tanimoto = KMeans(n_clusters = i, init = 'k-means++', max_iter = 400, n_init = 30)
+    kmeans_tanimoto.fit(m_tanimoto)
+    wcss_tanimoto.append(kmeans_tanimoto.inertia_)
+
+plt.plot(range(1,13), wcss_tanimoto)
+plt.title('Método do cotovelo')
+plt.xlabel('Número de clusters')
+plt.ylabel('Within-Clister-Sum-of-Squares')
+plt.show()
+
+# %%
+for i in range(2, 11):
+    clusterer = KMeans(n_clusters=i)
+    preds = clusterer.fit_predict(m_tanimoto)
+    score = silhouette_score(m_tanimoto, preds)
+    print('Silhueta para ' + str(i) + ' clusters : ' + str(score))
 
 # %% [markdown]
-# ##### 10-20
+# ### Supondo k=3  (Tanimoto)
 
 # %%
-draw_knn_graph(11,'jaccard','Red', mt_delicious)
-draw_knn_graph(12,'jaccard','Red', mt_delicious)
-draw_knn_graph(13,'jaccard','Red', mt_delicious)
-draw_knn_graph(14,'jaccard','Red', mt_delicious)
-draw_knn_graph(15,'jaccard','Red', mt_delicious)
+kmeans_tanimoto = KMeans(n_clusters = 3, init='k-means++', max_iter=300, n_init=30)
+clusters = kmeans.fit_predict(m_tanimoto)
+clusters  # consigo ver quem pertence a quais conjuntos
 
 # %%
-draw_knn_graph(16,'jaccard','Red', mt_delicious)
-draw_knn_graph(17,'jaccard','Red', mt_delicious)
-draw_knn_graph(18,'jaccard','Red', mt_delicious)
-draw_knn_graph(19,'jaccard','Red', mt_delicious)
-draw_knn_graph(20,'jaccard','Red', mt_delicious)
+x_tanimoto= m_tanimoto
 
-# %%
-draw_knn_graph(25,'jaccard','Red', mt_delicious)
+plt.figure(figsize=(8,8))
+plt.scatter(
+    x_tanimoto[clusters == 0,0], x_tanimoto[clusters == 0,1],
+    s=20, c='salmon',
+    #edgecolor='black',
+    label='Cluster A',
+)
 
-# %% [markdown]
-# ## 2.2) Rogers tanimoto
+plt.scatter(
+    x_tanimoto[clusters == 1,0], x_tanimoto[clusters == 1,1], 
+    s=40,c='violet',
+    #edgecolor='black',
+    label='Cluster B',
+    
+)
+plt.scatter(
+    x_tanimoto[clusters == 2,0], x_tanimoto[clusters == 2,1],
+    s=40,c='aqua',
+    #edgecolor='black',
+    label='CLuster C',
+)
 
-# %% [markdown]
-# #### Grafo completo
+# centroide
+plt.scatter(
+    kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1],
+    c='red',
+    edgecolor='black',
+    label='centroides',
+    s=90
+)
 
-# %%
-draw_graph('rogerstanimoto', 'Blue', mt_delicious)
-
-# %% [markdown]
-# #### Grafos esparsificados
-
-# %% [markdown]
-# ##### 1-10
-
-# %%
-draw_knn_graph(1,'rogerstanimoto', 'Blue', mt_delicious)
-draw_knn_graph(2,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(3,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(4,'rogerstanimoto', 'Blue', mt_delicious)
-draw_knn_graph(5,'rogerstanimoto','Blue',  mt_delicious)
-
-# %%
-draw_knn_graph(6,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(7,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(8,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(9,'rogerstanimoto', 'Blue', mt_delicious)
-draw_knn_graph(10,'rogerstanimoto', 'Blue', mt_delicious)
-
-# %% [markdown]
-# ##### 15-25
-
-# %%
-draw_knn_graph(15,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(20,'rogerstanimoto','Blue',  mt_delicious)
-draw_knn_graph(25,'rogerstanimoto','Blue', mt_delicious)
-
-# %% [markdown]
-# ## 2.3) Cityblock
-
-# %% [markdown]
-# #### Grafo completo
-
-# %%
-draw_graph('cityblock', 'purple', mt_delicious)
-
-# %% [markdown]
-# ### Grafos esparsificados
-
-# %% [markdown]
-# ##### 1 - 10
-
-# %%
-draw_knn_graph(1,'cityblock','Purple', mt_delicious)
-draw_knn_graph(2,'cityblock','Purple', mt_delicious)
-draw_knn_graph(3,'cityblock','Purple', mt_delicious)
-draw_knn_graph(4,'cityblock','Purple', mt_delicious)
-
-
-# %%
-draw_knn_graph(5,'cityblock','Purple', mt_delicious)
-draw_knn_graph(6,'cityblock','Purple', mt_delicious)
-draw_knn_graph(7,'cityblock','Purple', mt_delicious)
-draw_knn_graph(8,'cityblock','Purple', mt_delicious)
-draw_knn_graph(9,'cityblock','Purple', mt_delicious)
-draw_knn_graph(10,'cityblock','Purple', mt_delicious)
-
-
-# %% [markdown]
-# ##### 11-25
-
-# %%
-draw_knn_graph(11,'cityblock','Purple', mt_delicious)
-draw_knn_graph(12,'cityblock','Purple', mt_delicious)
-draw_knn_graph(13,'cityblock','Purple', mt_delicious)
-draw_knn_graph(14,'cityblock','Purple', mt_delicious)
-
-# %%
-draw_knn_graph(15,'cityblock','Purple', mt_delicious)
-
-
-# %%
-draw_knn_graph(16,'cityblock','Purple', mt_delicious)
-draw_knn_graph(17,'cityblock','Purple', mt_delicious)
-draw_knn_graph(18,'cityblock','Purple', mt_delicious)
-draw_knn_graph(19,'cityblock','Purple', mt_delicious)
-
-# %%
-draw_knn_graph(20,'cityblock','Purple', mt_delicious)
-
-
-# %%
-draw_knn_graph(25,'cityblock','Purple', mt_delicious)
-
-
-# %% [markdown]
-# ## 2.4) Chebyshev
-
-# %% [markdown]
-# #### Grafo completo
-
-# %%
-draw_graph('chebyshev', 'green', mt_delicious)
-
-# %% [markdown]
-# #### Grafos esparsificados
-
-# %%
-draw_knn_graph(1,'chebyshev','Green', mt_delicious)
-
-
-# %%
-draw_knn_graph(5,'chebyshev','Green', mt_delicious)
-
-
-# %% [markdown]
-# ## 2.5) Minkowski
-
-# %% [markdown]
-# #### Grafo completo 
-
-# %%
-draw_graph('minkowski','C0', mt_delicious)
-
-# %% [markdown]
-# #### Grafos esparsificados
-
-# %% [markdown]
-# ##### 1-10
-
-# %%
-draw_knn_graph(1, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(2, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(3, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(4, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(5, 'minkowski', 'C0', mt_delicious)
-
-# %%
-draw_knn_graph(6, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(7, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(8, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(9, 'minkowski', 'C0', mt_delicious)
-draw_knn_graph(10, 'minkowski', 'C0', mt_delicious)
-
-# %% [markdown]
-# ##  2.6) Hamming
-
-# %% [markdown]
-# - A distância de Hamming pode ser usada para medir a diferença entre duas sequências binárias de comprimento igual.
-# - A matriz m é uma matriz de distâncias que contém a distância de Hamming entre todas as combinações possíveis de linhas da matriz mt_delicious.
-
-# %% [markdown]
-# #### Grafo completo
-
-# %%
-draw_graph('hamming', 'cyan', mt_delicious)
-
-# %% [markdown]
-# #### Grafos esparsificados
-
-# %% [markdown]
-# ##### 1-10
-
-# %%
-draw_knn_graph(1,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(2,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(3,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(4,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(5,'hamming', 'cyan', mt_delicious)
-
-# %%
-draw_knn_graph(6,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(7,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(8,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(9,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(10,'hamming', 'cyan', mt_delicious)
-
-# %% [markdown]
-# ##### 15-20
-
-# %%
-draw_knn_graph(15,'hamming', 'cyan', mt_delicious)
-draw_knn_graph(20,'hamming', 'cyan', mt_delicious)
-
-# %% [markdown]
-# ## 2.7) Cosseno 
-
-# %% [markdown]
-# #### Grafo completo
-
-# %%
-draw_graph('cosine', 'C2', mt_delicious)
-
-# %% [markdown]
-# #### Grafos esparsificados 
-
-# %% [markdown]
-# ##### 1 - 10
-
-# %%
-draw_knn_graph(1, 'cosine', 'C2', mt_delicious)
-
-# %%
-draw_knn_graph(2, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(3, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(4, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(5, 'cosine', 'C2', mt_delicious)
-
-# %%
-draw_knn_graph(6, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(7, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(8, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(9, 'cosine', 'C2', mt_delicious)
-draw_knn_graph(10, 'cosine', 'C2', mt_delicious)
-
-
+plt.title('K-means utilizando distância tanimoto e k=3 na base Delicious.')
+plt.legend()
+plt.show()
